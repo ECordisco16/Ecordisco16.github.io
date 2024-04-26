02f2cf62c9677357e3fda9a3c7bb10fd0f3733f6
ECordisco16 <ecordisco@berklee.edu>
2024-04-26 16:24:24 -0400

    Friday Change

--- sketch.js
+++ sketch.js
@@ -1,18 +1,68 @@
-let img;
-function preload() {
-  img3 = loadImage('AK3.png');
+
+
+let img1, img2, img3;
+let song;
+var maxDiameter; 
+var theta; 
+
+//Images loading
+function preload(){
+  img1 = loadImage('AK1.jpeg');
+  img2 = loadImage('AK2.jpeg');
+  //img3 = loadImage('AK3.png');
+  
 }
 
 function setup() {
-  createCanvas(400, 400, WEBGL);
-
+  song = loadSound('PlayersAudio.wav');
+  createCanvas(400, 400);
+  
+// Pulsing cirlce variables  
+  maxDiameter = 30; 
+	theta = 200; 
+  
 }
 
 function draw() {
-  background(0);
-  rotateZ(frameCount * 0.01);
-  rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.01);
-  texture(img);
-  box(width / 2);
+  background(220);
+
+image(img1, 0, 0, width, height);
+//image(img2, 100, 100, width, height);
+  img2.resize(220, 220);
+  image(img2, 250, 250);
+
+  //pulsing circle 
+  noStroke();
+  
+var diam = 50 + sin(theta) * maxDiameter ;
+
+fill(255, 200, 200)
+ellipse(width/1.6,height/1.6, diam, diam); 
+
+  
+ theta += .02; 
+
+// Flower 
+  
+   fill(204, 101, 192, 127);
+  stroke(127, 63, 120);
+
+  translate(60, 60);
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 30, 20, 80);
+    rotate(PI/5);
+  }
+  
+}
+
+function mousePressed() {
+  if (song.isPlaying()) {
+
+    song.stop();
+   
+  } else {
+    song.play();
+    
+  }
 }
