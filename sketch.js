faff3ef0ff611623a6cd5069f272cdd1bff32e68
ECordisco16 <ecordisco@berklee.edu>
2024-05-07 15:04:52 -0400

    tuesday

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-let img1, img2, img3;
+let img1, img2, img3, img4, img5, img6;
 let song;
 let iconCube;
 var maxDiameter; 
@@ -9,6 +9,9 @@ function preload(){
   img1 = loadImage('AK1.jpeg');
   img2 = loadImage('AK2.jpeg');
   img3 = loadImage('AK3.png');
+  img4 = loadImage('A.png');
+  img5 = loadImage('K.png');
+  img6 = loadImage('Mirror.png');
   
 }
 
@@ -20,16 +23,44 @@ function setup() {
 // Pulsing cirlce variables  
   maxDiameter = 30; 
 	theta = 200; 
+
+   //maxDiameter = 30; 
+	//theta = 100; 
   
 }
 
 function draw() {
-  background(220);
+  //background(220);
+  
+  //flower 
+  ellipse(-200,-200,4000,4000);
+  //ellipse(100,115,30,30);
+ // ellipse(100,83,30,30);
+  //ellipse(80,100,30,30);
+  //fill(255, 230, 51);
+  //fill (225, random(225), random(225));
+  //ellipse(100,100,22,22); 
+  
+  
 
+  //anna 
 image(img1, -200, -200, width, height);
 //image(img2, 100, 100, width, height);
   img2.resize(220, 220);
   image(img2, 40, 40);
+  
+// Letters 
+
+image(img4, 55, -200,);
+  img4.resize(70, 70);
+image(img5, 120, -190,);
+  img5.resize(65, 65);
+  
+  //image(img5, 40, 40);
+  
+image(img6,-280,-0);
+  img6.resize(300,250)
+  
 
   //pulsing circle 
   noStroke();
@@ -39,15 +70,36 @@ var diam = 50 + sin(theta) * maxDiameter ;
 fill(255, 200, 200)
 ellipse(width/-3,height/-3, diam, diam); 
 
+ theta += .01; 
   
- theta += .02; 
+  
+  
+  noStroke();
+  
+var diam = 30 + sin(theta) * maxDiameter ;
+
+fill(195, 200, 200)
+ellipse(width/-2, height/-2.5, diam, diam); 
+
+ theta += .01; 
+
+  
+  noStroke();
+  
+var diam = 40 + sin(theta) * maxDiameter ;
+
+fill(225, 200, 200)
+ellipse(width/-3, height/-2, diam, diam); 
+
+ theta += .01; 
+
 
 // Flower 
   
    fill(204, 101, 192, 127);
   stroke(127, 63, 120);
 
-  translate(60, 60);
+  translate(80, 40);
   noStroke();
   for (let i = 0; i < 10; i ++) {
     ellipse(0, 30, 20, 80);
@@ -63,8 +115,12 @@ ellipse(width/-3,height/-3, diam, diam);
   rotateY(frameCount * 0.01);
   texture(img3);
   box(width / 5);
-  
+
+ 
 }
+
+
+
 
 //Play Song
 function mousePressed() {
